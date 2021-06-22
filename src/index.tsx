9a43725e7d0d28f0e8e8d9cd47c6367158399c06
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-06-22 19:52:42 -0300

    init styles home

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <h1>teste</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,27 +2,26 @@ import illustration from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIcon from "../assets/images/google-icon.svg";
 
+import "../styles/auth.scss";
+
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustration} alt="Ilustração" />
         <strong>Crie salas de Q&A ao vivo</strong>
         <strong>Tire dúvidas em tempo real</strong>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="logo" />
-          <button>
+          <button className="create-room">
             <img src={googleIcon} alt="google logo" />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
-
-              <input
-              type="text"
-               placeholder="digite o código da sala" />
+            <input type="text" placeholder="digite o código da sala" />
             <button type="submit">Entrar na sala</button>
           </form>
         </div>
