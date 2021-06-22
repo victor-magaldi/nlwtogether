28188801954e91a9bbcd6be5133b1fa8acb4598c
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-06-22 19:23:47 -0300

    add sass and clear styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Home } from './pages/Home';
 import firebase from "./services/firebase";
 
+import "./styles/global.scss"
+
 
 function App() {
   console.log(firebase)
