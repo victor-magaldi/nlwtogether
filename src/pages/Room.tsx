dbbba28d69bef54377606c9b59e148a0bdd60c15
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-06-23 20:10:08 -0300

    create page Room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import logoImg from "../assets/images/logo.svg"
+import "../styles/room.scss"
+
+import Button from '../components/Button'
 
 export default function Room() {
     return (
         <div>
-            sala
+            <div id="page-room">
+
+                <header>
+                    <div className="content">
+                        <img src={logoImg} alt="letmeask" />
+                        <div>
+                            código
+                        </div>
+                    </div>
+                </header>
+
+                <main >
+                    <div className="room-title">
+                        <h1>Sala React</h1>
+                        <span>
+                            4 perguntas
+                        </span>
+                    </div>
+
+                    <form >
+                        <textarea className="question"
+                            placeholder="o que você quer perguntar?"
+                        />
+                        <div className="form-footer">
+                            <span>Para enviar uma pergunta, <button>faça seu Login</button> </span>
+                            <Button type="submit"> Enviar pergunta</Button>
+
+                        </div>
+                    </form>
+
+                </main>
+            </div>
 
         </div>
     )
