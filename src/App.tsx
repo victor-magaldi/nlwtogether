453a5a88b851b3da122f1dc74d26fa8f4ca27bbe
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-06-22 21:20:14 -0300

    verificando se usuário está logado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 import { BrowserRouter, Route } from "react-router-dom";
 
@@ -23,6 +23,23 @@ export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
   const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+    
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+        if (!displayName || !photoURL) {
+          throw new Error("Missing information from Google Account");
+        }
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        });
+      }
+    });
+  }, []);
 
   async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
